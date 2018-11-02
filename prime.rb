cb3086fd0b08b95b658f4ee61d28fd45f3797099
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-02 04:04:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
 # Add  code here!
 def prime?(integar)
- integar{|i| return false if num % i == 0} #returns boolean of whether or not number is prime
+  return false if num <= 1
+  integar{|i| return false if num % i == 0} #returns boolean of whether or not number is prime
